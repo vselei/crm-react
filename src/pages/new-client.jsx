bedcbb88ed99f5af0483eb0886420e5c7d0a739e
Victor Seleimend <vsennaselei@gmail.com>
2023-01-30 14:31:14 -0300

    Botão de voltar na página de novos clientes

--- src/pages/new-client.jsx
+++ src/pages/new-client.jsx
@@ -1,7 +1,29 @@
-const NewClient = () => (
-  <div>
-    <h1>Novo Cliente</h1>
-  </div>
-);
+import { useNavigate } from 'react-router-dom';
+
+const NewClient = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Novo Cliente</h1>
+      <p className="mt-3">
+        Preencha todos os campos para registrar um novo cliente
+      </p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Voltar
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>Formulário</p>
+      </div>
+    </>
+  );
+};
 
 export default NewClient;
