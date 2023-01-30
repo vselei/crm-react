fc3a3aa16a8f703605a0edbf7034cd43481a9deb
Victor Seleimend <vsennaselei@gmail.com>
2023-01-30 13:22:37 -0300

    Adição de links

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Layout = () => (
   <div className="md:flex md:min-h-screen">
@@ -6,6 +6,11 @@ const Layout = () => (
       <h2 className="text-4xl font-black text-center text-white">
         CRM Clientes
       </h2>
+
+      <nav className='mt-10'>
+        <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/'>Clientes</Link>
+        <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/clients/new'>Novo Cliente</Link>
+      </nav>
     </aside>
     <main className="md:w-3/4 md:flex p-10 md:h-screen overflow-scroll">
       <Outlet />
