e23d8466d4fe367f78b6c8651f7b55b1e8ffbc56
Victor Seleimend <vsennaselei@gmail.com>
2023-01-30 18:16:14 -0300

    Requisição dos dados no JSON Server

---
+++ src/data/clients.jsx
@@ -0,0 +1,6 @@
+export const getClients = async () => {
+  const response = await fetch(import.meta.env.VITE_API_URL);
+  const result = await response.json();
+
+  return result;
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,45 +1,12 @@
 import { useLoaderData } from 'react-router-dom';
+
+import { getClients } from '../data/clients.jsx';
+
 import Client from '../components/Client';
 
 export const loader = () => {
-  const clients = [
-    {
-      id: 1,
-      name: 'Juan',
-      tel: 102013313,
-      email: 'juan@juan.com',
-      company: 'IBM'
-    },
-    {
-      id: 2,
-      name: 'Karen',
-      tel: 138198313,
-      email: 'karen@juan.com',
-      company: 'IBM'
-    },
-    {
-      id: 3,
-      name: 'Carlos',
-      tel: 31983913,
-      email: 'carlos@juan.com',
-      company: 'IBM'
-    },
-    {
-      id: 4,
-      name: 'Miguel',
-      tel: 319381983,
-      email: 'miguel@juan.com',
-      company: 'IBM'
-    },
-    {
-      id: 5,
-      name: 'Pedro',
-      tel: 1398198938,
-      email: 'pedro@juan.com',
-      company: 'IBM'
-    }
-  ];
-
+  const clients = getClients();
+  
   return clients;
 };
 
@@ -50,7 +17,7 @@ const Index = () => {
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administrar seus Clientes</p>
-      <div className='overflow-x-auto'>
+      <div className="overflow-x-auto">
         {clients.length ? (
           <table className="w-full bg-white shadow mt-5 table-auto">
             <thead className="bg-blue-800 text-white">
