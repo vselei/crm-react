995dda3ec5c7676e13b75cd5d6d7e7533730966b
Victor Seleimend <vsennaselei@gmail.com>
2023-01-30 13:54:46 -0300

    Obtendo clientes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,56 @@
+import { useLoaderData } from 'react-router-dom';
+
 export const loader = () => {
-  return 'Loader';
+  const clients = [
+    {
+      id: 1,
+      name: 'Juan',
+      tel: 102013313,
+      email: 'juan@juan.com',
+      company: 'IBM'
+    },
+    {
+      id: 2,
+      name: 'Karen',
+      tel: 138198313,
+      email: 'karen@juan.com',
+      company: 'IBM'
+    },
+    {
+      id: 3,
+      name: 'Carlos',
+      tel: 31983913,
+      email: 'carlos@juan.com',
+      company: 'IBM'
+    },
+    {
+      id: 4,
+      name: 'Miguel',
+      tel: 319381983,
+      email: 'miguel@juan.com',
+      company: 'IBM'
+    },
+    {
+      id: 5,
+      name: 'Pedro',
+      tel: 1398198938,
+      email: 'pedro@juan.com',
+      company: 'IBM'
+    }
+  ];
+
+  return clients;
 };
 
-const Index = () => (
-  <>
-    <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
-    <p className="mt-3">Administrar seus Clientes</p>
-  </>
-);
+const Index = () => {
+  const clients = useLoaderData();
+
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administrar seus Clientes</p>
+    </>
+  );
+};
 
 export default Index;
