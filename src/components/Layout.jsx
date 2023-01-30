66c27adaea7fb23a1ec31a39c98d2bc0a714d898
Victor Seleimend <vsennaselei@gmail.com>
2023-01-30 13:32:54 -0300

    Estilização dos links ativos

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,21 +1,43 @@
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
-const Layout = () => (
-  <div className="md:flex md:min-h-screen">
-    <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
-      <h2 className="text-4xl font-black text-center text-white">
-        CRM Clientes
-      </h2>
+const Layout = () => {
+  const location = useLocation();
 
-      <nav className='mt-10'>
-        <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/'>Clientes</Link>
-        <Link className='text-2xl block mt-2 hover:text-blue-300 text-white' to='/clients/new'>Novo Cliente</Link>
-      </nav>
-    </aside>
-    <main className="md:w-3/4 md:flex p-10 md:h-screen overflow-scroll">
-      <Outlet />
-    </main>
-  </div>
-);
+  console.log(location);
+
+  return (
+    <div className="md:flex md:min-h-screen">
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM Clientes
+        </h2>
+
+        <nav className="mt-10">
+          <Link
+            className={`${
+              location.pathname === '/' ? 'text-blue-300' : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300 `}
+            to="/"
+          >
+            Clientes
+          </Link>
+          <Link
+            className={`${
+              location.pathname === '/clients/new'
+                ? 'text-blue-300'
+                : 'text-white'
+            } text-2xl block mt-2 hover:text-blue-300 `}
+            to="/clients/new"
+          >
+            Novo Cliente
+          </Link>
+        </nav>
+      </aside>
+      <main className="md:w-3/4 md:flex p-10 md:h-screen overflow-scroll">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
 
 export default Layout;
